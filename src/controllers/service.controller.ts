575efa967b4ae0a8e803f11fc61f700408708534
Dev-Rayyan Ahmed <rayyanahmed1624@gmail.com>
2026-06-21 17:33:27 +0500

    added: get & patch functions

--- src/controllers/service.controller.ts
+++ src/controllers/service.controller.ts
@@ -3,7 +3,7 @@ import { db } from "../db/db.js";
 import { serviceTypes, services } from "../db/schema/index.js";
 import ApiError from "../utils/api-error.js";
 import ApiResponse from "../utils/api-response.js";
-import { eq } from "drizzle-orm";
+import { eq, ilike } from "drizzle-orm";
 
 type ServiceType = typeof serviceTypes.$inferSelect;
 type Service = typeof services.$inferSelect;
@@ -138,6 +138,139 @@ export const getServicesByServiceType = async (
     if ((error as any).code === "22P02") {
       return next(new ApiError(400, "BAD_REQUEST", "Invalid Service Type ID format."));
     }
+    next(error);
+  }
+};
+
+export const getAllServiceTypes = async (
+  _: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const allTypes = await db.query.serviceTypes.findMany();
+
+    return res.status(200).json(
+      new ApiResponse<ServiceType[]>(
+        200, 
+        allTypes, 
+        "Service categories fetched successfully"
+      )
+    );
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateServiceType = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const id = req.params.id as string;
+    const { name, isQueuingEnabled, doctorInvolvement, description } = req.body;
+
+    if (!id) {
+      throw new ApiError(400, "BAD_REQUEST", "Service Type ID is required.");
+    }
+
+    const [updatedType] = await db
+      .update(serviceTypes)
+      .set({
+        ...(name && { name: name}),
+        ...(isQueuingEnabled !== undefined && { isQueuingEnabled }),
+        ...(doctorInvolvement && { doctorInvolvement }),
+        ...(description !== undefined && { description }),
+      })
+      .where(eq(serviceTypes.id, id))
+      .returning();
+
+    if (!updatedType) {
+      throw new ApiError(404, "NOT_FOUND", "Service category not found.");
+    }
+
+    return res.status(200).json(
+      new ApiResponse<ServiceType>(200, updatedType, "Category updated successfully")
+    );
+  } catch (error) {
+    if ((error as any).code === "22P02") {
+      return next(new ApiError(400, "BAD_REQUEST", "Invalid ID format."));
+    }
+    if ((error as any).code === "23505") {
+      return next(new ApiError(409, "CONFLICT", "A category with this name already exists."));
+    }
+    next(error);
+  }
+};
+
+export const updateService = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const id = req.params.id as string;
+    const { serviceTypeId, serviceName, basePrice } = req.body;
+
+    if (!id) {
+      throw new ApiError(400, "BAD_REQUEST", "Service ID is required.");
+    }
+
+    const [updatedService] = await db
+      .update(services)
+      .set({
+        ...(serviceTypeId && { serviceTypeId }),
+        ...(serviceName && { serviceName }),
+        ...(basePrice && { basePrice: basePrice.toString() }),
+      })
+      .where(eq(services.id, id))
+      .returning();
+
+    if (!updatedService) {
+      throw new ApiError(404, "NOT_FOUND", "Service not found.");
+    }
+
+    return res.status(200).json(
+      new ApiResponse<Service>(200, updatedService, "Service updated successfully")
+    );
+  } catch (error) {
+    if ((error as any).code === "22P02") {
+      return next(new ApiError(400, "BAD_REQUEST", "Invalid ID format."));
+    }
+    if ((error as any).code === "23503") {
+      return next(new ApiError(404, "NOT_FOUND", "The provided Category ID does not exist."));
+    }
+    next(error);
+  }
+};
+
+export const searchServices = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const searchQuery = req.query.q as string;
+
+    if (!searchQuery) {
+      return res.status(200).json(new ApiResponse(200, [], "Empty search"));
+    }
+
+    const searchTerm = `%${searchQuery}%`;
+
+    const matches = await db.query.services.findMany({
+      where: ilike(services.serviceName, searchTerm),
+      with: { 
+        serviceType: true 
+      },
+      limit: 10,
+    });
+
+    return res.status(200).json(
+      new ApiResponse(200, matches, "Search results fetched")
+    );
+  } catch (error) {
     next(error);
   }
 };
